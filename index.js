bbdabdccb04bff6d81b73c8360f487ffd8627488
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-08 14:41:50 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
-
+const icecreamActions =
+  require("./features/icecream/icecreamSlice").icecreamActions;
 console.log("Initial State ", store.getState());
 
 const unsubscribe = store.subscribe(() => {
@@ -12,5 +13,12 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+
+store.dispatch(icecreamActions.restocked(4));
 
 unsubscribe();
